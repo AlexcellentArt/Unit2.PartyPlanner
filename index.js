85b1d421aaae695795217087c39d8e3568f6fba5
Alexis <alexcellentart@gmail.com>
2024-08-12 21:19:50 -0400

    Prettied up the visuals.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const COHORT = "2407-FTB-ET-WEB-FT";
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events `;
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
 
 const state = {
   parties: [],
-  //   inputs: { invalid: [], valid: [], neutral: [] },
   inputs: {},
 };
 
@@ -14,9 +13,6 @@ const addPartyForm = document.querySelector("#addParty");
 addPartyForm.addEventListener("submit", addParty);
 addPartyForm.addEventListener("input", updateFormInput);
 
-// const inputs = addPartyForm.querySelectorAll("input")
-// inputs.forEach((input) => {input.addEventListener("input", function (event){validateInput(event.target.value,event.target.type)});})
-
 async function render() {
   await getParties();
   renderParties();
@@ -25,7 +21,6 @@ async function render() {
 render();
 
 async function getParties() {
-  // TODO
   try {
     const response = await fetch(API_URL);
     console.log(response);
@@ -38,7 +33,6 @@ async function getParties() {
 }
 
 function renderParties() {
-  // TODO
   if (!state.parties.length) {
     partiesList.innerHTML =
       /*html*/
@@ -48,120 +42,107 @@ function renderParties() {
   console.log("parties found");
   console.log(state.parties);
   const partiesUI = state.parties.map((party) => {
+    //make and fill out ui
     const ui = document.createElement("li");
     ui.innerHTML = /*html*/ `
     <h2>${party.name}</h2>
     <h3>${party.date}</h2>
     <h3>${party.location}</h2>
     <p>${party.description}</p>`;
+
+    // make delete button
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete Party";
     ui.append(deleteButton);
-    deleteButton.addEventListener("click", () => deleteParty);
+    deleteButton.addEventListener("click", () => deleteParty(party.id));
     return ui;
   });
-  // id description imageUrl
   partiesList.replaceChildren(...partiesUI);
 }
 
-/**
- * Ask the API to create a new artist based on form data
- * @param {Event} event
- */
 async function addParty(event) {
   event.preventDefault();
   const name = event.target[0].value;
   const location = event.target[1].value;
-  //   const time = event.target[2].value;
   const date = new Date(event.target[2].value);
   const description = event.target[3].value;
-  //    Time: ${time}
   console.log(
     `Name: ${name} Location: ${location} Date: ${date} Desc: ${description}`
   );
-//   if (
-//     name === "" ||
-//     location === "" ||
-//     description === "" ||
-//     isNaN(date) === false
-//   ) {
-//     console.log("ERROR");
-//     return;
-//   }
 
-// Check if inputs are valid before going through with post
-let errorMsg = '';
-const inputs = [event.target[0],event.target[1],event.target[2],event.target[3]]
-for (i in inputs){
-    if (validateInput(inputs[i].value,inputs[i].type) === false){
-        errorMsg += ","+inputs[i].name
+  // Check if inputs are valid before going through with post
+  let errorMsg = "";
+  const inputs = [
+    event.target[0],
+    event.target[1],
+    event.target[2],
+    event.target[3],
+  ];
+  for (i in inputs) {
+    if (validateInput(inputs[i].value, inputs[i].type) === false) {
+      errorMsg += "," + inputs[i].name;
     }
-}
-// Error message will only not be blank if an input was found invalid, so after printing the error message, return, thus aborting the function
-if (errorMsg != '')
-{
-    console.error("These inputs are invalid, aborting addToParty: "+errorMsg.substring(1))
-    return
-}
-  // check if only numbers
+  }
+  // Error message will only not be blank if an input was found invalid, so after printing the error message, return, thus aborting the function
+  if (errorMsg != "") {
+    console.error(
+      "These inputs are invalid, aborting addToParty: " + errorMsg.substring(1)
+    );
+    return;
+  }
 
   console.log(name);
-  // TODO
   try {
-    const response = await fetch(API_URL,{
-      method:"POST",
-      headers: {"Content-Type":"application/json"},
-      body:JSON.stringify({date,description,location,name,}),
-      //names, dates, times, locations, and descriptions
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ date, description, location, name }),
     });
-    const json = await response.json()
-    console.log(json)
+    const json = await response.json();
+    console.log(json);
+
+    //clean form
+    for (i in inputs) {
+      inputs[i].value = "";
+    }
+    // reset class to neutral state
+    const keys = Object.keys(state.inputs);
+    for (i in keys) {
+      state.inputs[keys[i]] = inputState.NEUTRAL;
+    }
   } catch (error) {
     console.error("%c" + error, "color:red");
   }
 }
-function deleteParty() {
+async function deleteParty(id) {
   console.log("DELETE PARTY PRESSED");
+  try {
+    const response = await fetch(`${API_URL}/${id}`, { method: "DELETE" });
+    console.log(response.status);
+    if (!response.ok) {
+      throw new Error("Party couldn't be deleted!");
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
 function updateFormInput(event) {
   console.log(event.target);
   console.log(event.target.type);
-  console.log(event.target.id + "is ID")
+  console.log(event.target.id + "is ID");
   const isValid = validateInput(event.target.value, event.target.type);
-//   let addToKey = "";
-//   let removeFromKey = "";
   if (isValid) {
     state.inputs[event.target.id] = inputState.VALID;
   } else {
     state.inputs[event.target.id] = inputState.INVALID;
   }
-  // get which key to move from
-  //   if (isValid) {
-  //     addToKey = "valid";
-  //     removeFromKey = "invalid";
-  //   } else {
-  //     addToKey = "invalid";
-  //     removeFromKey = "valid";
-  //   }
-  //   // handle moving inputs
-  //   if (state.inputs[addToKey].findIndex(event.target.id) === -1) {
-  //     state.inputs[addToKey].push(event.target.id);
-  //   }
-  //   const removeIdx = state.inputs[removeFromKey].findIndex(event.target.id);
-  //   if (removeIdx !== -1) {
-  //     state.inputs[removeFromKey].splice(removeIdx,1);
-  //   }
   render();
 }
 function validateInput(value, type) {
-  // console.log(event.target)
-  // console.log(event.target.type)
-  // event.target.type === "date"
-  // console.log(
-  //   `Name: ${name} Location: ${location} Date: ${date} Desc: ${description}`
-  // );
   let isValid = true;
-  if (type === "submit"){return true}
+  if (type === "submit") {
+    return true;
+  }
   if (type === "date") {
     if (isNaN(new Date(value)) === true) {
       isValid = false;
@@ -174,10 +155,10 @@ function validateInput(value, type) {
   return isValid;
 }
 function updateInputClasses(target, state) {
-  console.log(`Updating input state for ${target} to ${state}`)
-  console.log(state)
+  console.log(`Updating input state for ${target} to ${state}`);
+  console.log(state);
   if (state == inputState.VALID) {
-    console.log("VALID")
+    console.log("VALID");
     target.classList.add("valid");
     target.classList.remove("invalid");
     target.classList.remove("neutral");
@@ -185,28 +166,21 @@ function updateInputClasses(target, state) {
     target.classList.add("invalid");
     target.classList.remove("valid");
     target.classList.remove("neutral");
-    console.log("INVALID")
+    console.log("INVALID");
   } else if (state == inputState.NEUTRAL) {
     target.classList.remove("invalid");
     target.classList.remove("valid");
     target.classList.add("neutral");
-    console.log("Neutral")
+    console.log("Neutral");
   }
 }
 function renderInputs() {
-    console.table(state.inputs)
+  console.table(state.inputs);
   const keys = Object.keys(state.inputs);
   for (i in keys) {
-    updateInputClasses(addPartyForm.querySelector("#"+keys[i]),state.inputs[keys[i]]);
+    updateInputClasses(
+      addPartyForm.querySelector("#" + keys[i]),
+      state.inputs[keys[i]]
+    );
   }
 }
-// if (
-//     name === "" ||
-//     location === "" ||
-//     description === "" ||
-//     isNaN(date) === false
-//   )
-//   {
-//     console.log("ERROR");
-//     return;
-//   }
